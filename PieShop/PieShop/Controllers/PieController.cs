d4073ebc1061b0930a15eea14c5fa9b685d5acd7
Kamil Sałęga <kamil.salega@pcgacademia.pl>
2020-06-23 01:40:24 +0200

    KER-002 - Stworzenie kontrolera

---
+++ PieShop/PieShop/Controllers/PieController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
