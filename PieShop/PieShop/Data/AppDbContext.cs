b85dee77ba831571237d432c39870ca1f0ed30c1
Kamil Sałęga <kamil.salega@pcgacademia.pl>
2020-06-24 01:13:34 +0200

    KER-003 - Dodawanie pakietów i tworzenie kontekstu

---
+++ PieShop/PieShop/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PieShop.Models;
+
+namespace PieShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        
+    }
+}
