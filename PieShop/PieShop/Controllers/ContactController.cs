b12ef8b0c6abdf7f6ff35621b69c955a46d4ef03
Kamil <kamilsalega@gmail.com>
2020-06-28 22:02:42 +0200

    KER-005 - Utworznie strony kontaktowej

--- PieShop/PieShop/Controllers/ContactController.cs
+++ PieShop/PieShop/Controllers/ContactController.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PieShop.Controllers
 {
-    public class ContactController
+    public class ContactController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
